bacbb1dd22964168e54571fb8bb1aee90f34aa77
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-19 14:57:32 +0530

    Add files via upload

---
+++ prodmult.java
@@ -0,0 +1,19 @@
+package player;
+
+import java.util.Scanner;
+
+public class prodmult {
+public static void main(String[] args) {
+	Scanner rd=new Scanner(System.in);
+	int sz=rd.nextInt();
+	int arr[]=new int[sz];
+	for(int i=0;i<sz;i++){
+		arr[i]=rd.nextInt();
+	}
+	int tot=Math.abs(arr[0]*arr[1]);
+	for(int i=2;i<sz;i++){
+		tot=Math.abs(tot*arr[i]);
+	}
+	System.out.println(tot);
+}
+}
