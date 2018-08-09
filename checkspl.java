ce53501d4f9ebf78f115d28f13cc0c920e40442d
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-09 21:54:33 +0530

    Add files via upload

---
+++ checkspl.java
@@ -0,0 +1,25 @@
+package set1;
+
+import java.util.Scanner;
+public class checkspl{
+ public static void main(String args[]){
+   Scanner in=new Scanner(System.in);
+   String s=in.next();
+   char c[]=s.toCharArray();
+   int cnt=0,cnt1=0;
+    for(int i=0;i<s.length();i++){
+    if(c[i]==40){
+          cnt++;
+           }
+       if(c[i]==41){
+           cnt1++;
+           } 
+        }
+ if(cnt==cnt1){
+  System.out.println("yes");
+  }
+   else{
+   System.out.println("no");
+   }
+}
+}
