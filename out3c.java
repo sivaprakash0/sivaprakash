7200d1a77b667b44fc8f43d005c849c2fd0b3900
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-10 12:01:51 +0530

    Update out3c.java

--- out3c.java
+++ out3c.java
@@ -1,15 +1,11 @@
-package set3;
-
 import java.util.Scanner;
-
 public class out3c {
 public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
 	String s=in.next();
 	String s1=Character.toString(s.charAt(0));
 	for(int i=3;i<s.length();i=i+3){
-		s1=s1+s.substring(i, i+1);
-		
+		s1=s1+s.substring(i, i+1);	
 	}
 	System.out.println(s1);
 }
