aeb38283a59262d7217011eeb89c7e6c1b7b67e0
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-13 17:37:38 +0530

    Add files via upload

---
+++ findnumeric.java
@@ -0,0 +1,19 @@
+package set3;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class findnumeric {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String no=in.next();int cnt = 0;
+	for(int i=0;i<no.length();i++){
+		char c[]=no.toCharArray();
+		if(c[i]>='0' && c[i]<='9'){
+			cnt++;
+		}
+	}
+	if(cnt==no.length()){System.out.println("yes");}
+	else{System.out.println("no");}
+}
+}
